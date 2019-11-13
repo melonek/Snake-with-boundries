03d2afb081a38d81224a659c67731da2db44f4c6
melon <meloniusz@hotmail.com>
2019-11-13 23:10:00 +0800

    working on js//pause//day1

--- snake.js
+++ snake.js
@@ -1,5 +1,5 @@
 const cvs = document.getElementById("snake");
-const ctx = csv.getContext("2d");
+const ctx = cvs.getContext("2d");
 
 //create the unit
 const box = 32;
@@ -7,34 +7,41 @@ const box = 32;
 //load images
 
 const ground = new Image();
-ground.src = "img/ground.png"
+ground.src = "img/ground.png";
 
-const food = new Image();
-food.src = "img/food.png"
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
 //create the snake
 
 let snake = [];
 snake[0] = {
-    x : 9 * box,
-    y : 10 * box
-}
-    //create the food
+  x: 9 * box,
+  y: 10 * box
+};
+//create the food
 let food = {
-    x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*15+3) * box
-
-}
+  x: Math.floor(Math.random() * 17 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 3) * box
+};
 
 // create the score var
 let score = 0;
 
 //draw everything to the canvas
 
-function (draw) {
-    ctx.drawImage(ground,0,0);
+function draw() {
+  ctx.drawImage(ground, 0, 0);
+
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i == 0 ? "green" : "white";
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+  }
 }
 
 //call the draw function every 100ms
 
-let game = setInterval (draw, 100);
+let game = setInterval(draw, 100);
